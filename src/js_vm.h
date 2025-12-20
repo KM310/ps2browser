4b1349e2b9dab814024ca6d53f859a8c2c217097
K_M <julius@marcel-loos.de>
2025-12-21 00:25:04 +0100

    Update js_vm.h

--- src/js_vm.h
+++ src/js_vm.h
@@ -1,7 +1,10 @@
 #ifndef JS_VM_H
 #define JS_VM_H
 
-// (for later) later there is a JS-support
-void js_run_placeholder(void);
+#include "html_parser.h"
+
+void js_run_placeholder(const char *code);
+void js_run_external(const char *src);
+void execute_scripts(HtmlNode *root);
 
 #endif
