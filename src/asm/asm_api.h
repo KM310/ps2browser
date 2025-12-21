0a39af0ae534d29ad4905ac60056a621d6f4cbae
K_M <julius@marcel-loos.de>
2025-12-21 01:46:03 +0100

    Add files via upload

---
+++ src/asm/asm_api.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Beispiel-Funktionen – trage hier deine echten ASM-Funktionen ein
+int fast_strlen(const char* str);
+void fast_memcpy(void* dst, const void* src, int len);
+
+#ifdef __cplusplus
+}
+#endif
