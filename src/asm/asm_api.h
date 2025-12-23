f2799e2414b6301bb3bce06f139b7f871dd7d0c2
K_M <julius@marcel-loos.de>
2025-12-23 01:25:55 +0100

    Update asm_api.h

--- src/asm/asm_api.h
+++ src/asm/asm_api.h
@@ -1,13 +1,4 @@
 #pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Beispiel-Funktionen – trage hier deine echten ASM-Funktionen ein
-int fast_strlen(const char* str);
-void fast_memcpy(void* dst, const void* src, int len);
-
-#ifdef __cplusplus
-}
-#endif
+void* mips_fast_memcpy(void* dst, const void* src, unsigned int size);
+unsigned int mips_fast_strlen(const char* s);
